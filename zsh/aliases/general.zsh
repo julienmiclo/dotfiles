2072d8bb80c7e6c88ec1aab487b128f397244364
julienmiclo <julienmiclo@gmail.com>
2020-12-01 15:30:38 +0100

    update zsh

--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -13,6 +13,8 @@ alias wifi="sudo wifi-menu"
 alias load-keyboard="~/.dotfiles/scripts/.scripts/keyboard/load_apple_keyboard.sh"
 alias unload-keyboard="~/.dotfiles/scripts/.scripts/keyboard/unload_apple_keyboard.sh"
 
+alias load-aukey="~/.dotfiles/scripts/.scripts/aukey.sh"
+
 alias clear-cache="trizen -Sc --noconfirm"
 
 alias ssh="TERM=xterm-256color ssh"
